9581f20f4f36be52996301c143c7501860e5eae2
wh250 <wh250@scarletmail.rutgers.edu>
2017-05-13 16:41:30 -0400

    Add files via upload

--- scheduler.c
+++ scheduler.c
@@ -15,7 +15,6 @@ using namespace std;
 queue<int> rrsh;
 //low priority queue
 queue<int> rrs;
-int procid=-1;
 //if there are high priority processes
 bool high;
 //interrupts time quantum
@@ -122,22 +121,101 @@ void* readl(void* arg){
 	}
 }
 
+//create processes
+void create_process(int argc, char** argv) {
+	string temp;
+	char* args[] = { (char*)"blank" };
+	int pid;
+	//create all child processes
+	for (int i = 1; i<argc; i++) {
+		temp = argv[i];
+		//add ./ to run it
+		temp = "./" + temp;
+		//fork and exec
+		pid = fork();
+		args[0] = (char*)temp.c_str();
+		if (pid == 0) {
+			execvp(temp.c_str(), args);
+			cout << "failed" << endl;
+		}
+		else {
+			//add process id to queue, default low
+			rrs.push(pid);
+		}
+	}
+}
+
+void stopall(queue<int>& qq) {
+	for (int i = 0; i < qq.size(); i++) {
+		kill(qq.front(), SIGSTOP);
+		qq.push(qq.front());
+		qq.pop();
+	}
+}
+
+void checkinput() {
+	//if there is input
+	if (inputk != "") {
+		//check what input is and call corespondind function
+		//input must be "h", "l", "h pid", or "l pid"
+		if (inputk == "h") {
+			kill(getpid(), SIGUSR1);
+		}
+		if (inputk == "l") {
+			kill(getpid(), SIGUSR2);
+		}
+		if (inputk.length() >= 3) {
+			if (inputk[0] == 'h' && inputk[1] == ' ') {
+				kill(getpid(), SIGUSR1);
+			}
+			if (inputk[0] == 'l' && inputk[1] == ' ') {
+				kill(getpid(), SIGUSR2);
+			}
+		}
+		inputk = "";
+	}
+}
+
+//run for 1 time quantum or until interrupted
+void runtq(queue<int> qq) {
+	//time measurement
+	time_t start, end;
+	//start first process in queue
+	kill(qq.front(), SIGCONT);
+	//set start time of current time quantum
+	start = time(NULL);
+	end = time(NULL);
+	//run for 1 time quantum or until interrupted
+	while (difftime(end, start)<tq && !brake) {
+		end = time(NULL);
+		//if process finished in time quantum
+		if (waitpid(qq.front(), &status, WNOHANG)>0) {
+			//remove process and set next process to run
+			qq.pop();
+			kill(qq.front(), SIGCONT);
+			//interrupt
+			brake = true;
+		}
+	}
+	//if process didn't finish and time quantum ended
+	//stop current process, move to back of queue, start next one
+	if (!brake) {
+		kill(qq.front(), SIGSTOP);
+		qq.push(qq.front());
+		qq.pop();
+		kill(qq.front(), SIGCONT);
+	}
+}
+
 //Round robin process scheduler
 //Takes input in form of ./scheduler procname1 procname2 etc
 //Has high and low priorities, settable from shell commands
 //all processes are run as children for 1 time quantum. 
 //High priority processes go first
 int main(int argc, char** argv){
-	string temp;
-	//time measurement
-	time_t start, end;
 	int status;
-	//argument strings
-	char* args[]={(char*)"blank"};
 	//thread to check input
 	pthread_t tipt;
-	//pid for forking
-	int pid;
 	int *a=new int;
 	//time quantum is 10 seconds
 	float tq=10;
@@ -146,127 +224,30 @@ int main(int argc, char** argv){
 	signal(SIGUSR2,setl);
 	//make input thread
 	pthread_create(&tipt,NULL,&readl,(void*) a);
-	
-	//create all child processes
-	for(int i=1;i<argc;i++){
-		temp=argv[i];
-		//add ./ to run it
-		temp="./"+temp;
-		//fork and exec
-		pid=fork();
-		args[0]=(char*)temp.c_str();
-		if(pid==0){
-			execvp(temp.c_str(),args);
-			cout<<"failed"<<endl;
-		}
-		else{
-			//add process id to queue, default low
-			rrs.push(pid);
-		}
-	}
-	
+	//create processes
+	create_process(argc, argv);
 	//Stop all child processes
-	for(int i=0;i<rrs.size();i++){
-		kill(rrs.front(),SIGSTOP);
-		rrs.push(rrs.front());
-		rrs.pop();
-	}
+	stopall(rrs);
 	
 	//if there are processes
 	while(rrs.size()>0 || rrsh.size()>0){
-		//if there is input
-		if(inputk!=""){
-			//check what input is and call corespondind function
-			//input must be "h", "l", "h pid", or "l pid"
-			if(inputk=="h"){
-				kill(getpid(),SIGUSR1);
-			}
-			if(inputk=="l"){
-				kill(getpid(),SIGUSR2);
-			}
-			if(inputk.length()>=3){
-				if(inputk[0]=='h' && inputk[1]==' '){
-					kill(getpid(),SIGUSR1);
-				}
-				if(inputk[0]=='l' && inputk[1]==' '){
-					kill(getpid(),SIGUSR2);
-				}
-			}
-			inputk="";
-		}
-		//high is if there are high priority processes
-		high=!rrsh.empty();
+		checkinput();
 		//interrupt is false
 		brake=false;
-		
 		//stop all processes in queue
 		if(!rrsh.empty()){
-			for(int i=0;i<rrsh.size();i++){
-				kill(rrsh.front(),SIGSTOP);
-				rrsh.push(rrsh.front());
-				rrsh.pop();
-			}
+			stopall(rrsh);
 		}
-		
 		if(!rrs.empty()){
-			for(int i=0;i<rrs.size();i++){
-				kill(rrs.front(),SIGSTOP);
-				rrs.push(rrs.front());
-				rrs.pop();
-			}
+			stopall(rrs);
 		}
-		
 		//if there are high priority processes
-		if(high){
-			//start first process in queue
-			kill(rrsh.front(),SIGCONT);
-			//set start time of current time quantum
-			start=time(NULL);
-			end=time(NULL);
-			//run for 1 time quantum or until interrupted
-			while(difftime(end,start)<tq && !brake){
-				end=time(NULL);
-				//if process finished in time quantum
-				if(waitpid(rrsh.front(),&status,WNOHANG)>0){
-					//remove process and set next process to run
-					rrsh.pop();
-					kill(rrsh.front(),SIGCONT);
-					//interrupt
-					brake=true;
-				}
-			}
-			//if process didn't finish and time quantum ended
-			//stop current process, move to back of queue, start next one
-			if(!brake){
-				kill(rrsh.front(),SIGSTOP);
-				rrsh.push(rrsh.front());
-				rrsh.pop();
-				kill(rrsh.front(),SIGCONT);
-			}
+		if(!rrsh.empty()){
+			runtq(rrsh);
 		}
 		//otherwise run low priority process queue
 		else{
-			//run first process in queue
-			kill(rrs.front(),SIGCONT);
-			start=time(NULL);
-			end=time(NULL);
-			//for 1 time quantum and not interrupted
-			while(difftime(end,start)<tq && !brake){
-				end=time(NULL);
-				//same thing for other queue
-				if(waitpid(rrs.front(),&status,WNOHANG)>0){
-					rrs.pop();
-					kill(rrs.front(),SIGCONT);
-					brake=true;
-				}
-			}
-			//same thing for other queue
-			if(!brake){
-				kill(rrs.front(),SIGSTOP);
-				rrs.push(rrs.front());
-				rrs.pop();
-				kill(rrs.front(),SIGCONT);
-			}
+			runtq(rrs);
 		}
 	}
 	//if all processes are finished
